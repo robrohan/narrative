aa5d6dd39a9c79dc2df1e5be3ebfd950cbda4f62
Rob Rohan <rob@therohans.com>
2022-04-11 19:32:46 +1200

    Adding command line args and test data

--- cmd/narrative/main.go
+++ cmd/narrative/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/ardanlabs/conf"
 )
 
 var build = "develop"
 
+type Config struct {
+	Start     string `conf:"short:s,default:/*"`
+	End       string `conf:"short:e,default:*/"`
+	MdComment string `conf:"default:'    '"`
+	Input     string `conf:"short:i,required"`
+	Output    string `conf:"short:o,default:final.md"`
+}
+
 func run() error {
+	log := log.New(os.Stdout, "NT: ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+
+	cfg := Config{}
+	if err := conf.Parse(os.Args[1:], "NT", &cfg); err != nil {
+		if err == conf.ErrHelpWanted {
+			usage, err := conf.Usage("NT", &cfg)
+			if err != nil {
+				return err
+			}
+			fmt.Println(usage)
+			return nil
+		}
+		return err
+	}
+
+	log.Printf("%v", cfg)
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module robrohan.com/narrative
 
 go 1.18
+
+require github.com/ardanlabs/conf v1.5.0
